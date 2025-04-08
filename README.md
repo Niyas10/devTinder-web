a80ecde3b0ad2d535db45b424ef430e308345742
Niyas10 <niyazmuhammad688@gmail.com>
2025-04-08 10:40:39 +0530

    routes created

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 create a vite + react application 
 removed unwanted files 
 install tailwind css
+install daisy ui 
+craete footer 
+create a routes 
+create a login  page 
---
+++ src/routes/UserRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import  Home from "../pages/Home";
+import Login from "../pages/Login";
+
+
+const UserRoute = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login />} />
+     
+ 
+    </Routes>
+  );
+};
+
+export default UserRoute;
